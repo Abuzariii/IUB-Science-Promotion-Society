a4c94c795e52eba99a1ef88a2c60d26701f8cb4a
Muhammad Abuar <muhammadabuzar010@gmail.com>
2023-03-31 15:04:16 +0500

    Executive Members

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { Fragment } from "react";
 import Footer from "./components/Footer/Footer";
 import Cover from "./components/Cover/Cover";
 import CoreTeam from "./components/Body/Core Team/CoreTeam";
+import Executives from "./components/Body/Executive Members/Executives";
 
 function App() {
   return (
     <Fragment>
       <Cover />
       <CoreTeam />
+      <Executives />
       <Footer />
     </Fragment>
   );
---
+++ src/components/Body/Executive Members/Executives.jsx
@@ -0,0 +1,58 @@
+import classes from "./Executives.module.css";
+import { motion } from "framer-motion";
+
+export default function Executives() {
+  return (
+    <div className={classes.execs}>
+      <motion.h1
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.5 }}
+      >
+        Executive Members.
+      </motion.h1>
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 1 }}
+      >
+        <div className={classes.ct1}>
+          <div>
+            <h4>Aiman Bukhari</h4>
+            <h6>Dept. of Chemistry</h6>
+          </div>
+        </div>
+        <div className={classes.ct2}>
+          <div>
+            <h4>Qamar Zaman</h4>
+            <h6>Dept. of Zoology</h6>
+          </div>
+        </div>
+        <div className={classes.ct3}>
+          <div>
+            <h4>M. Bilal Khalid</h4>
+            <h6>Dept. of Agriculture</h6>
+          </div>
+        </div>
+        <div className={classes.ct4}>
+          <div>
+            <h4>Moeen Akhtar</h4>
+            <h6>Dept. of Chemistry</h6>
+          </div>
+        </div>
+        <div className={classes.ct5}>
+          <div>
+            <h4>Sidra Qasim</h4>
+            <h6>Dept. of Zoology</h6>
+          </div>
+        </div>
+        <div className={classes.ct6}>
+          <div>
+            <h4>Marwa Yasmeen</h4>
+            <h6>Dept. of Botany</h6>
+          </div>
+        </div>
+      </motion.div>
+    </div>
+  );
+}
